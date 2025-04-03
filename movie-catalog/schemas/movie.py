7f3560c32d25951f3774ddff5ad19069ac65abde
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-04 06:55:10 +0900

    Create MovieCreate schema

--- movie-catalog/schemas/movie.py
+++ movie-catalog/schemas/movie.py
@@ -1,9 +1,22 @@
+from typing import Annotated
+
+from annotated_types import Len
+
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
-    id: int
+    id: int | None = None
     title: str
+    description: str
+    year: int
+    duration: int
+
+
+class MovieCreate(BaseModel):
+    """Модель для создания фильма"""
+
+    title: Annotated[str, Len(min_length=8, max_length=50)]
     description: str
     year: int
     duration: int
