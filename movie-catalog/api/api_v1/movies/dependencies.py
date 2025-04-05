837230e952dd45fa2f876be972157adfb41e042d
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-05 09:11:04 +0900

    Replace movie_details with movie in prefetch_movie

--- movie-catalog/api/api_v1/movies/dependencies.py
+++ movie-catalog/api/api_v1/movies/dependencies.py
@@ -8,9 +8,9 @@ from schemas.movie import Movie
 def prefetch_movie(
     slug: str,
 ) -> Movie:
-    movie_details: Movie | None = storage.get_by_slug(slug=slug)
-    if movie_details:
-        return movie_details
+    movie: Movie | None = storage.get_by_slug(slug=slug)
+    if movie:
+        return movie
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Movie {slug!r} not found",
