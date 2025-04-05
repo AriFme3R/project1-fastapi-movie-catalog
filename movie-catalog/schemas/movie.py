02e47d6c0503211962746f0c5500a575be23c659
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-05 14:16:07 +0900

    Create MovieUpdate schema

--- movie-catalog/schemas/movie.py
+++ movie-catalog/schemas/movie.py
@@ -1,14 +1,19 @@
 from typing import Annotated
 
-from annotated_types import Len
+from annotated_types import (
+    Len,
+    MaxLen,
+)
 
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
-    slug: str | None = None
     title: str
-    description: str
+    description: Annotated[
+        str,
+        MaxLen(200),
+    ] = ""
     year: int
     duration: int
 
@@ -16,13 +21,32 @@ class MovieBase(BaseModel):
 class MovieCreate(MovieBase):
     """Модель для создания фильма"""
 
-    title: Annotated[str, Len(min_length=8, max_length=50)]
+    slug: Annotated[
+        str,
+        Len(min_length=5, max_length=50),
+    ]
+    title: Annotated[
+        str,
+        Len(min_length=5, max_length=50),
+    ]
     description: str
     year: int
     duration: int
+
+
+class MovieUpdate(MovieBase):
+    """
+    Модель для обновления информации о фильме
+    """
+    description: Annotated[
+        str,
+        MaxLen(200),
+    ]
 
 
 class Movie(MovieBase):
     """
     Модель фильма
     """
+
+    slug: str
