77d8bdeb0d024222864cad6c1d1f2e680e714bc4
Nikolay Ivashchenko <programswork2@gmail.com>
2025-05-17 19:07:08 +0900

    Add AbstractTokensHelper

--- movie-catalog/api/api_v1/movies/redis.py
+++ movie-catalog/api/api_v1/movies/redis.py
@@ -1,3 +1,9 @@
+import secrets
+from abc import (
+    ABC,
+    abstractmethod,
+)
+
 from redis import Redis
 
 from core import config
@@ -9,3 +15,43 @@ redis_tokens = Redis(
     db=config.REDIS_DB_FOR_TOKENS,
     decode_responses=True,
 )
+
+
+class AbstractTokensHelper(ABC):
+    """
+    Что мне нужно от обертки:
+    - проверка наличия токена
+    - добавление токена в хранилище
+    - генерация нового токена
+    """
+
+    @abstractmethod
+    def token_exists(
+        self,
+        token: str,
+    ) -> bool:
+        """
+        Check if token exists.
+        :param token:
+        :return:
+        """
+
+    @abstractmethod
+    def add_token(
+        self,
+        token: str,
+    ) -> None:
+        """
+        Save token to storage.
+        :param token:
+        :return:
+        """
+
+    @classmethod
+    def generate_token(cls) -> str:
+        return secrets.token_urlsafe(16)
+
+    def generate_and_save_token(self) -> str:
+        token = self.generate_token()
+        self.add_token(token)
+        return token
