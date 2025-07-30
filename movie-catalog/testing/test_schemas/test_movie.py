eba9540adc97f3229168487033c2577427233fa3
Nikolay Ivashchenko <programswork2@gmail.com>
2025-07-30 18:33:26 +0900

    Create MovieUpdateTestCase

--- movie-catalog/testing/test_schemas/test_movie.py
+++ movie-catalog/testing/test_schemas/test_movie.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from schemas.movie import (
     Movie,
     MovieCreate,
+    MovieUpdate,
 )
 
 
@@ -23,3 +24,23 @@ class MovieCreateTestCase(TestCase):
         self.assertEqual(movie_in.year, movie.year)
         self.assertEqual(movie_in.duration, movie.duration)
         self.assertEqual(movie_in.description, movie.description)
+
+
+class MovieUpdateTestCase(TestCase):
+    def test_movie_can_be_updated_from_update_schema(self) -> None:
+        update_data = MovieUpdate(
+            title="some-title",
+            year=2025,
+            duration=90,
+            description="some-description",
+        )
+
+        movie = Movie(
+            **update_data.model_dump(),
+            slug="some-slug",
+        )
+
+        self.assertEqual(update_data.title, movie.title)
+        self.assertEqual(update_data.year, movie.year)
+        self.assertEqual(update_data.duration, movie.duration)
+        self.assertEqual(update_data.description, movie.description)
