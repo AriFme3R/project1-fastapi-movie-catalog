6177ba8f8ad6e627635069f600b8cf5d7f296f08
Nikolay Ivashchenko <programswork2@gmail.com>
2025-07-31 10:15:52 +0900

    Create MoviePartialUpdateTestCase

--- movie-catalog/testing/test_schemas/test_movie.py
+++ movie-catalog/testing/test_schemas/test_movie.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from schemas.movie import (
     Movie,
     MovieCreate,
+    MoviePartialUpdate,
     MovieUpdate,
 )
 
@@ -44,3 +45,43 @@ class MovieUpdateTestCase(TestCase):
         self.assertEqual(movie_in.year, movie.year)
         self.assertEqual(movie_in.duration, movie.duration)
         self.assertEqual(movie_in.description, movie.description)
+
+
+class MoviePartialUpdateTestCase(TestCase):
+    def test_empty_partial_update_from_partial_update_schema(self) -> None:
+        movie_in = MoviePartialUpdate()
+
+        movie = Movie(
+            **movie_in.model_dump(exclude_unset=True),
+            slug="some-slug",
+            title="some-title",
+            year=2025,
+            duration=90,
+            description="some-description",
+        )
+
+        self.assertEqual("some-title", movie.title)
+        self.assertEqual(2025, movie.year)
+        self.assertEqual(90, movie.duration)
+        self.assertEqual("some-description", movie.description)
+
+    def test_movie_can_be_partial_update_from_partial_update_schema(
+        self,
+    ) -> None:
+        movie_in = MoviePartialUpdate(
+            title="another-title",
+            year=2020,
+        )
+
+        movie = Movie(
+            **movie_in.model_dump(exclude_unset=True),
+            slug="some-slug",
+            duration=90,
+            description="some-description",
+        )
+
+        self.assertEqual(movie_in.title, movie.title)
+        self.assertEqual(movie_in.year, movie.year)
+
+        self.assertEqual(90, movie.duration)
+        self.assertEqual("some-description", movie.description)
