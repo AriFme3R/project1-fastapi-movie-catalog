5c6ef92b1f9940fb7391e521698bf5f24c8806f6
Nikolay Ivashchenko <programswork2@gmail.com>
2025-08-01 12:26:49 +0900

    Edit config for testing

--- movie-catalog/core/config.py
+++ movie-catalog/core/config.py
@@ -1,4 +1,5 @@
 import logging
+from os import getenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -6,11 +7,13 @@ MOVIES_STORAGE_FILEPATH = BASE_DIR / "movies.json"
 
 
 LOG_LEVEL = logging.INFO
-LOG_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+LOG_FORMAT = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+)
 
 
 REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_PORT = int(getenv("REDIS_PORT", "0")) or 6379
 REDIS_DB = 0
 REDIS_DB_FOR_TOKENS = 1
 REDIS_DB_FOR_USERS = 2
